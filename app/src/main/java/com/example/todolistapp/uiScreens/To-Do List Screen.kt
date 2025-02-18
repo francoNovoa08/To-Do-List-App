5768c00c3f271cdf56cda533f4456f32241351e9
Franco Novoa <franconovoa3@gmail.com>
2025-02-17 23:33:33 -0500

    Add task builder function

--- app/src/main/java/com/example/todolistapp/uiScreens/To-Do List Screen.kt
+++ app/src/main/java/com/example/todolistapp/uiScreens/To-Do List Screen.kt
@@ -1,31 +1,68 @@
 package com.example.todolistapp.uiScreens
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ToDoListScreen() {
-    var tasksList = remember { mutableStateListOf<String>() }
+    var tasksList = remember { mutableStateListOf<String>("Hello", "Potato") }
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .statusBarsPadding()
+            .statusBarsPadding(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        for (task in tasksList) {
+            BuildTask(task)
+            Spacer(modifier = Modifier.height(10.dp))
+        }
     }
 }
 
-fun buildTask(task: String) {
-
+@Composable
+fun BuildTask(task: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 48.dp, vertical = 16.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .border(1.dp, Color.Black.copy(alpha = 0.5f), RoundedCornerShape(12.dp))
+            .padding(12.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = task,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.Start)
+                .padding(6.dp),
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp
+        )
+    }
 }
 
 
