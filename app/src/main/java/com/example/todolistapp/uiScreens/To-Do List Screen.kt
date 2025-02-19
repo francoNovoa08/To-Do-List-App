abdf6d7efcea93f080e96675006c4da4ee7f6d84
Franco Novoa <franconovoa3@gmail.com>
2025-02-18 21:36:56 -0500

    Add start of bottom sheet UI

--- app/src/main/java/com/example/todolistapp/uiScreens/To-Do List Screen.kt
+++ app/src/main/java/com/example/todolistapp/uiScreens/To-Do List Screen.kt
@@ -1,5 +1,6 @@
 package com.example.todolistapp.uiScreens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,9 +10,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
@@ -20,23 +32,71 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.todolistapp.ui.theme.LightBlue
 
 @Composable
 fun ToDoListScreen() {
     var tasksList = remember { mutableStateListOf<String>("Hello", "Potato") }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .statusBarsPadding(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .statusBarsPadding()
     ) {
-        for (task in tasksList) {
-            BuildTask(task)
-            Spacer(modifier = Modifier.height(10.dp))
+        LazyColumn(
+            modifier = Modifier.weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(tasksList) { task ->
+                BuildTask(task)
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+    }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(
+                    topStart = 50.dp,
+                    topEnd = 50.dp,
+                    bottomStart = 0.dp,
+                    bottomEnd = 0.dp
+                ))
+                .background(LightBlue)
+                .height(200.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(modifier = Modifier.height(90.dp))
+            Button(onClick = {
+                println("hello")
+            },
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = LightBlue,
+                    containerColor = Color.White
+                ),
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add task",
+                    tint = LightBlue,
+                    modifier = Modifier.requiredSize(55.dp)
+                )
+            }
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                text = "Add new task",
+                fontSize = 40.sp,
+                fontFamily = FontFamily.SansSerif,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
@@ -60,7 +120,7 @@ fun BuildTask(task: String) {
                 .wrapContentWidth(Alignment.Start)
                 .padding(6.dp),
             fontWeight = FontWeight.Bold,
-            fontSize = 32.sp
+            fontSize = 24.sp
         )
     }
 }
